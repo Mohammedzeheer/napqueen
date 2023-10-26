fdfee848891ef610f28085cc249650e089a0fa25
Mohammed <zeheerzak@gmail.com>
2023-10-26 21:51:38 +0530

    added cors

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,29 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 require('./config/mongoose');
 require('dotenv').config();
+const cors = require('cors');
 
-var usersRouter = require('./routes/post');
+const usersRouter = require('./routes/post');
 
-var app = express();
+let app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_SIDE_URL,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    credentials: true,
+  })
+);
 
 app.use('/', usersRouter);
 
--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -1,63 +1,145 @@
-const postModel= require('../models/postModel')
+const postModel = require('../models/postModel');
 
-const getblog= async(req, res) => {
-    try {
-      const posts = await postModel.find();
-      res.json(posts);
-    } catch (error) {
-      res.status(500).json({ message: error.message });
-    }
-  };
-  
-const getblogById =  async (req, res) => {
-    try {
-      const post = await postModel.findById(req.params.id);
-      res.json(post);
-    } catch (error) {
-      res.status(500).json({ message: error.message });
-    }
-}
+const getblog = async (req, res) => {
+  try {
+    const posts = await postModel.find();
+    res.status(200).json(posts); 
+  } catch (error) {
+    res.status(500).json({ message: error.message }); 
+  }
+};
 
+const getblogById = async (req, res) => {
+  try {
+    const post = await postModel.findById(req.params.id);
+    if (post) {
+      res.status(200).json(post); 
+    } else {
+      res.status(404).json({ message: 'Post not found' }); 
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message }); 
+  }
+};
 
 const addBlog = async (req, res) => {
-    const post = new postModel({
-      title: req.body.title,
-      content: req.body.content,
-      category_id: req.body.category_id,
-    });
+  const post = new postModel({
+    title: req.body.title,
+    content: req.body.content,
+    category_id: req.body.category_id,
+  });
 
- try {
+  try {
     const newPost = await post.save();
-    res.status(201).json(newPost);
+    res.status(201).json(newPost); 
   } catch (error) {
-    res.status(400).json({ message: error.message });
+    res.status(400).json({ message: error.message }); 
+  }
+};
+
+const editBlog = async (req, res) => {
+  try {
+    const post = await postModel.findByIdAndUpdate(
+      req.params.id,
+      { title: req.body.title, content: req.body.content, updated_at: Date.now() },
+      { new: true }
+    );
+    if (post) {
+      res.status(200).json(post); 
+    } else {
+      res.status(404).json({ message: 'Post not found' }); 
+    }
+  } catch (error) {
+    res.status(400).json({ message: error.message }); 
   }
 };
 
 
-const editBlog = async (req, res) => {
-    try {
-      const post = await postModel.findByIdAndUpdate(
-        req.params.id,
-        { title: req.body.title, content: req.body.content, updated_at: Date.now() },
-        { new: true }
-      );
-      res.json(post);
-    } catch (error) {
-      res.status(400).json({ message: error.message });
+const deleteBlog = async (req, res) => {
+  try {
+    const deletedPost = await postModel.findByIdAndDelete(req.params.id);
+    if (deletedPost) {
+      res.status(200).json({ message: 'Post deleted' }); 
+    } else {
+      res.status(404).json({ message: 'Post not found' }); 
     }
+  } catch (error) {
+    res.status(500).json({ message: error.message }); 
   }
+};
 
-  const deleteBlog = async (req, res) => {
-        try {
-          await postModel.findByIdAndDelete(req.params.id);
-          res.json({ message: 'Post deleted' });
-        } catch (error) {
-          res.status(500).json({ message: error.message });
-        }
-   }
+module.exports = {
+  getblog,
+  getblogById,
+  addBlog,
+  editBlog,
+  deleteBlog
+};
 
 
-  module.exports={
-    getblog,getblogById, addBlog , editBlog ,deleteBlog
-  }
+
+
+
+// const postModel= require('../models/postModel')
+
+// const getblog= async(req, res) => {
+//     try {
+//       const posts = await postModel.find();
+//       res.json(posts);
+//     } catch (error) {
+//       res.status(500).json({ message: error.message });
+//     }
+//   };
+  
+// const getblogById =  async (req, res) => {
+//     try {
+//       const post = await postModel.findById(req.params.id);
+//       res.json(post);
+//     } catch (error) {
+//       res.status(500).json({ message: error.message });
+//     }
+// }
+
+
+// const addBlog = async (req, res) => {
+//     const post = new postModel({
+//       title: req.body.title,
+//       content: req.body.content,
+//       category_id: req.body.category_id,
+//     });
+
+//  try {
+//     const newPost = await post.save();
+//     res.status(201).json(newPost);
+//   } catch (error) {
+//     res.status(400).json({ message: error.message });
+//   }
+// };
+
+
+// const editBlog = async (req, res) => {
+//     try {
+//       const post = await postModel.findByIdAndUpdate(
+//         req.params.id,
+//         { title: req.body.title, content: req.body.content, updated_at: Date.now() },
+//         { new: true }
+//       );
+//       res.json(post);
+//     } catch (error) {
+//       res.status(400).json({ message: error.message });
+//     }
+//   }
+
+//   const deleteBlog = async (req, res) => {
+//         try {
+//           await postModel.findByIdAndDelete(req.params.id);
+//           res.json({ message: 'Post deleted' });
+//         } catch (error) {
+//           res.status(500).json({ message: error.message });
+//         }
+//    }
+
+
+//   module.exports={
+//     getblog,getblogById, addBlog , editBlog ,deleteBlog
+//   }
